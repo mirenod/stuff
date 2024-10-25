d1c335b40bed8cec1795708624b6d01a28050332
mireno <39898635+mirenod@users.noreply.github.com>
2024-10-25 16:09:19 +0100

    Update dynamic-select.sh

--- dynamic-select.sh
+++ dynamic-select.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -eu
+exec 3>&2
 
 # Read JSON file and parse options
-options="$(jq -r '.options[]' options.json)"$'\nQuit'
+select_menu_dynamic() {
+set +eu
+local options="$1"$'\nQuit'
+local IFS
+local options_array
+local opt
 
 # Convert options into an array
 IFS=$'\n' read -r -d '' -a options_array < <(echo "$options"$'\0')
@@ -10,14 +17,21 @@ select opt in "${options_array[@]}"
 do
     case $opt in
         "Quit")
-            echo "Exiting the script."
-            break
+            echo "Exiting the script." >&3
+            exit 0
             ;;
         *)
-            echo "You chose $opt"
+            echo "You chose $opt" >&3
             # Place commands or function calls for the chosen option here
             break
             ;;
     esac
 done
-echo "End $opt"
+
+set -eu
+echo "$opt"
+}
+
+result=$(menu "$(jq -r '.options[]' options.json)")
+result=$(select_menu_dynamic $'Something\nSomething\nSomething')
+echo "End $result"
